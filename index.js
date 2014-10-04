02175197dff784e5edf26c30643aa5823f78e10b
LenaKari <olena.levitsky@gmail.com>
2014-10-04 16:29:40 +0100

    Removed style attributes to be set in css

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var container = document.createElement("div");
-container.style.width = "200px";
-container.style.height = "100px";
 
 document.body.appendChild(container);
 
@@ -10,7 +8,6 @@ document.onkeydown = function(e) {
     if (document.getElementById(e.keyCode)) {return;}
     var span = document.createElement("span");
     span.innerHTML = String.fromCharCode(e.keyCode);
-    span.setAttribute("style", "width:20px; height:20px; border: 1px solid black; border-radius:5px;padding: 4px;");
 
     span.setAttribute("id", e.keyCode);
     container.appendChild(span);
